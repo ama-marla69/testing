4d5ea4d24842d523455f36b9eb019e8292754d03
ama-marla69 <ozazou2001@gmail.com>
2020-10-07 11:24:42 +0200

    Extra canvi

--- index.php
+++ index.php
@@ -2,3 +2,4 @@ echo 'Al gener,tanca la porta i encen el braser';
 echo 'Al gener,tanca la porta';
 echo 'Aigua de gener tot l'any va bé';
 echo 'Les gelades de gener fan florir l'ametller';
+“Extra canvi”
